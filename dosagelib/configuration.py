a5108dfb45ed7068b8c9b577d02ed3215b689902
Bastian Kleineidam <calvin@debian.org>
2012-10-12 22:15:40 +0200

    Fix copyright.

--- dosagelib/configuration.py
+++ dosagelib/configuration.py
@@ -11,8 +11,10 @@ Author = configdata.author
 HtmlAuthor = Author.replace(u' ', u'&nbsp;')
 Maintainer = configdata.maintainer
 HtmlMaintainer = Maintainer.replace(u' ', u'&nbsp;')
-Copyright = u"Copyright (C) 2004-2008 "+Author+u", (C) 2012 "+Maintainer
-HtmlCopyright = u"Copyright &copy; 2004-2008 "+HtmlAuthor+u", 2012 "+HtmlMaintainer
+Copyright = u"Copyright (C) 2004-2008 " + \
+  (u",".join(Author.split(",")[:2]))+u"  (C) 2012 "+Maintainer
+HtmlCopyright = u"Copyright &copy; 2004-2008 " + \
+  (u",".join(HtmlAuthor.split(",")[:2]))+u" &copy; 2012 "+HtmlMaintainer
 Url = configdata.url
 SupportUrl = Url + u"/issues"
 Email = configdata.maintainer_email
