052e5100851d569129bc4d9547bb58794f360b65
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-02-06 06:58:06 +0100

    Added HijinksEnsue comic strip.

--- dosagelib/plugins/h.py
+++ dosagelib/plugins/h.py
@@ -5,6 +5,14 @@ from ..scraper import _BasicScraper
 from ..util import tagre
 
 
+class HijinksEnsue(_BasicScraper):
+    url = 'http://hijinksensue.com/'
+    stripUrl = url + '%s/'
+    imageSearch = compile(tagre("img", "src", r'(http://hijinksensue\.com/comics/\d+-\d+-\d+[^"]+)'))
+    prevSearch = compile(tagre("a", "href", r'(http://hijinksensue\.com/\d+/\d+/\d+/[^"]+)', after="navi-prev"))
+    help = 'Index format: yyyy/mm/dd/name'
+
+
 class HorribleVille(_BasicScraper):
     url = 'http://horribleville.com/'
     stripUrl = url + 'd/%s.html'
