1c270a251c2fc9d758d15d9cf8311796f3c78118
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-02-17 08:26:44 +0100

    Increase Travis tests.

--- tests/test_comics.py
+++ tests/test_comics.py
@@ -91,7 +91,7 @@ def generate_comic_testers():
     g = globals()
     if "TRAVIS" in os.environ:
         # get limited number of scraper tests on Travis builds
-        max_scrapers = 20
+        max_scrapers = 40
         scraperclasses = islice(scraper.get_scraperclasses(), 0, max_scrapers)
     else:
         scraperclasses = scraper.get_scraperclasses()
