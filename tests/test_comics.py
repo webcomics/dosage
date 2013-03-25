bc6da819761180f8a397ac8f847f2230234f3c78
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-03-25 19:49:29 +0100

    Only 1 test for travis.

--- tests/test_comics.py
+++ tests/test_comics.py
@@ -127,7 +127,7 @@ def generate_comic_testers():
     g = globals()
     if "TRAVIS" in os.environ:
         # Get limited number of scraper tests on Travis builds.
-        max_scrapers = 10
+        max_scrapers = 1
         scraperclasses = islice(scraper.get_scraperclasses(), 0, max_scrapers)
     else:
         scraperclasses = scraper.get_scraperclasses()
