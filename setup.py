a1860e15d9f9744eb0b32f207702ed9368799e0c
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-04-02 22:58:21 +0200

    Bump up version.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from distutils.command.register import register
 from distutils import util
 from distutils.file_util import write_file
 
-AppVersion = '1.15'
+AppVersion = '2.0'
 AppName = 'Dosage'
 
 py_excludes = ['doctest', 'unittest', 'Tkinter', 'pdb',
