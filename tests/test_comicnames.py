13963d8bfe06746ebf5c3715e781a90ace7c580e
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-05-22 22:29:29 +0200

    Improve comicname error message.

--- tests/test_comicnames.py
+++ tests/test_comicnames.py
@@ -19,4 +19,4 @@ class TestComicNames(TestCase):
             else:
                 comicname = name
             self.assertEqual(util.asciify(comicname), comicname)
-            self.assertTrue(isinstance(scraperclass.description, text_type), comicname)
+            self.assertTrue(isinstance(scraperclass.description, text_type), "Invalid description in %s: %r" % (scraperclass, scraperclass.description))
