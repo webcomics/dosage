78414cb3d75501346c25c6b38b599f9c040f2913
Tobias Gruetzmacher <tobias-git@23.gs>
2015-04-20 22:46:12 +0200

    Switch from PIL to Pillow, remove workarounds.

--- scripts/install_pil.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Install PIL in Travis CI environment for Python 2.x builds.
-set -e
-set -u
-
-if python -c 'import sys; sys.exit(0 if sys.hexversion<0x03000000 else 1)'; then
-  pip install --allow-external PIL --allow-unverified PIL PIL
-fi
