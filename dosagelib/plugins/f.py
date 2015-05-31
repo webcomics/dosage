3ea82362240cdf379ff5282cb9fc8af0d917c7e5
Damjan Košir <damjan.kosir@gmail.com>
2015-05-31 23:29:34 +1200

    fixed FowlLanguage

--- dosagelib/plugins/f.py
+++ dosagelib/plugins/f.py
@@ -125,15 +125,6 @@ class ForLackOfABetterComic(_BasicScraper):
     help = 'Index format: number'
 
 
-class FowlLanguage(_BasicScraper):
-    url = 'http://www.fowllanguagecomics.com/'
-    stripUrl = url + 'comic/%s'
-    firstStripUrl =  stripUrl % 'part-of-the-process'
-    imageSearch = compile(tagre("img", "src", r'(http://i\d\.wp\.com/www\.fowllanguagecomics\.com/wp-content/uploads/\d+/\d+/[^"]+\.[a-z]+).*'))
-    prevSearch = compile(tagre("a", "href", r'([^"]+)', after="comic-nav-previous"))
-    help = 'Index format: yyyy/mm/stripname'
-
-
 class Freefall(_BasicScraper):
     url = 'http://freefall.purrsia.com/default.htm'
     stripUrl = 'http://freefall.purrsia.com/ff%s/fc%s.htm'
