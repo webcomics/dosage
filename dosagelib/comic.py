b35d521e489b839654ce48457f887e8c657f82eb
Peter Janes <git@peterjanes.ca>
2016-04-02 13:12:52 -0400

    Send downloaded event when skipping, to allow multiple executions in a day

--- dosagelib/comic.py
+++ dosagelib/comic.py
@@ -79,6 +79,7 @@ class ComicImage(object):
         # compare with >= since content length could be the compressed size
         if os.path.isfile(fn) and os.path.getsize(fn) >= self.contentLength:
             out.info(u'Skipping existing file "%s".' % fn)
+            getHandler().comicDownloaded(self, fn, text=self.text)
             return fn, False
         content = self.urlobj.content
         if not content:
