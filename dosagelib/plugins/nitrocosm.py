e4650d594148fbf8852d77d29f0e73ee7c4514c1
Tobias Gruetzmacher <tobias-git@23.gs>
2016-05-21 14:35:53 +0200

    Remove make_scraper from Nitrocosm.

--- dosagelib/plugins/nitrocosm.py
+++ dosagelib/plugins/nitrocosm.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
-from ..scraper import make_scraper, _ParserScraper
+# Copyright (C) 2004-2005 Tristan Seligmann and Jonathan Jacobs
+# Copyright (C) 2012-2014 Bastian Kleineidam
+# Copyright (C) 2015-2016 Tobias Gruetzmacher
+
+from __future__ import absolute_import, division, print_function
+
+from ..scraper import _ParserScraper
 
 
-def add(name, relativeUrl):
-    attrs = dict(
-        name=name,
-        url='http://www.nitrocosm.com/go/' + relativeUrl,
-        imageSearch='//img[@class="gallery_display"]',
-        prevSearch='//a[@class="nav_btn_previous"]'
-    )
-    globals()[name] = make_scraper(name, _ParserScraper, **attrs)
+class Nitrocosm(_ParserScraper):
+    imageSearch = '//img[@class="gallery_display"]'
+    prevSearch = '//a[@class="nav_btn_previous"]'
 
+    def __init__(self, name, path):
+        super(Nitrocosm, self).__init__(name)
+        self.url = 'http://www.nitrocosm.com/go/' + path
 
-add('2214', '2214_classic/')
-add('OTE', 'ote/')
-add('ProperBarn', 'gag/')
+    @classmethod
+    def getmodules(cls):
+        return [
+            cls('2214', '2214_classic/'),
+            cls('OTE', 'ote/'),
+            cls('ProperBarn', 'gag/'),
+        ]
