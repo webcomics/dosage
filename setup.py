c87653015956b5cda049b12d3d95c0fa9f747725
Tobias Gruetzmacher <tobias-git@23.gs>
2016-10-15 20:22:59 +0200

    Require "new" version of setuptools and pbr.
    See
    http://docs.openstack.org/developer/pbr/compatibility.html#evaluate-marker

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Copyright (C) 2004-2005 Tristan Seligmann and Jonathan Jacobs
 # Copyright (C) 2012-2014 Bastian Kleineidam
-# Copyright (C) 2015 Tobias Gruetzmacher
+# Copyright (C) 2015-2016 Tobias Gruetzmacher
+
+from __future__ import absolute_import, division, print_function
 
 from setuptools import setup
 
 setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
 )
