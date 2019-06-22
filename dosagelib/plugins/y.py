70223bd38febee81c2bec7590505f7f35c6dd4b6
Techwolf <dring@wolfishly.me>
2019-06-22 00:16:21 -0700

    Add YouSayItFirst

--- dosagelib/plugins/y.py
+++ dosagelib/plugins/y.py
@@ -4,8 +4,20 @@
 # Copyright (C) 2015-2016 Tobias Gruetzmacher
 
 from __future__ import absolute_import, division, print_function
+
+from ..scraper import _BasicScraper, _ParserScraper
 from .common import _WordPressScraper
 
 
 class YAFGC(_WordPressScraper):
     url = 'http://yafgc.net/'
+
+
+class YouSayItFirst(_ParserScraper):
+    stripUrl = 'https://www.yousayitfirst.com/comics/index.php?date=%s'
+    url = stripUrl % '20130125'
+    firstStripUrl = stripUrl % '20040220'
+    imageSearch = '//a/img'
+    prevSearch = '//a[text()="Previous"]'
+    endOfLife = True
+    help = 'Index format: yyyymmdd'
