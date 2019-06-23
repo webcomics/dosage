ff4fa7892d1c31edb9bdaa9c2c23dda21050dd1d
Techwolf <dring@wolfishly.me>
2019-06-23 14:36:33 -0700

    Add QuantumVibe

--- dosagelib/plugins/q.py
+++ dosagelib/plugins/q.py
@@ -6,8 +6,16 @@
 from __future__ import absolute_import, division, print_function
 
 from re import compile, escape
-from ..scraper import _BasicScraper
+from ..scraper import _BasicScraper, _ParserScraper
 from ..util import tagre
+
+
+class QuantumVibe(_ParserScraper):
+    url = 'https://www.quantumvibe.com/'
+    stripUrl = url + 'strip?page=%s'
+    firstStripUrl = stripUrl % '1'
+    imageSearch = '//img[contains(@src, "disppageV3?story=qv")]'
+    prevSearch = '//a[./img[contains(@src, "nav/prevstrip")]]'
 
 
 class QuestionableContent(_BasicScraper):
