1e9440d79cc9087dda633c0245edb54a407f1869
Mikkel Hoegh <mikkel@hoegh.org>
2019-06-27 20:05:18 +0200

    Fix missing returns in RSS parsing
    Also added a very basic test for feed parsing.

---
+++ tests/test_rss.py
@@ -0,0 +1,21 @@
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import re
+import time
+from dosagelib.rss import parseFeed
+
+class TestFeed(object):
+    """
+    Tests for rss.py
+    """
+
+    def test_parseFeed(self):
+        testTime = time.localtime(1560000000.0)
+        feed = parseFeed('./tests/mocks/dailydose.rss', testTime)
+
+        xmlBlob = feed.getXML()
+
+        assert u'PlumedDotage - 4034.png'.encode() in xmlBlob
+        assert u'PachinkoParlor - 20190626.jpg'.encode() not in xmlBlob
