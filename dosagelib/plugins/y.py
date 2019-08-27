e54d153ca771bbc53808b644cbf04425ffab967e
Techwolf <dring@wolfishly.me>
2019-08-27 00:54:52 -0700

    Add YoshSaga

--- dosagelib/plugins/y.py
+++ dosagelib/plugins/y.py
@@ -5,8 +5,17 @@
 
 from __future__ import absolute_import, division, print_function
 
+from ..scraper import _ParserScraper
 from .common import _WordPressScraper
 
 
 class YAFGC(_WordPressScraper):
     url = 'http://yafgc.net/'
+
+
+class YoshSaga(_ParserScraper):
+    url = 'https://www.yoshsaga.com/'
+    stripUrl = url + 'comic/%s/'
+    firstStripUrl = stripUrl % 'introduction'
+    imageSearch = '//div[@class="webcomic-image"]//img'
+    prevSearch = '//a[contains(@class, "previous-webcomic-link")]'
