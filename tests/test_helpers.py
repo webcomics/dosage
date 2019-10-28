d243017affa1397a8a08f34b3c1a01177f6f315e
Tobias Gruetzmacher <tobias-git@23.gs>
2019-10-28 19:17:57 +0100

    Add test for recently added helper

---
+++ tests/test_helpers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Tobias Gruetzmacher
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import re
+from dosagelib.helpers import joinPathPartsNamer
+
+
+class TestNamer(object):
+    """
+    Tests for comic namer.
+    """
+
+    def test_joinPathPartsNamer(self):
+        imgurl = 'https://HOST/wp-content/uploads/2019/02/tennis5wp-1.png'
+        pageurl = 'https://HOST/2019/03/11/12450/'
+        assert joinPathPartsNamer((0, 1, 2))(self, imgurl, pageurl) == '2019_03_11_tennis5wp-1.png'
+        assert joinPathPartsNamer((0, 1, 2), (-1,), '-')(self, imgurl, pageurl) == '2019-03-11-tennis5wp-1.png'
+        assert joinPathPartsNamer((0, -2), ())(self, imgurl, pageurl) == '2019_12450'
