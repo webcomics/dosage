d0bc0572c2e9864c3fbf4a18b82be6bc06dd348c
Tobias Gruetzmacher <tobias-git@23.gs>
2019-12-29 20:50:56 +0100

    Add a test to ckeck that renamed comics point to existing comics

--- tests/test_comicnames.py
+++ tests/test_comicnames.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2004-2008 Tristan Seligmann and Jonathan Jacobs
 # Copyright (C) 2012-2014 Bastian Kleineidam
-# Copyright (C) 2015-2017 Tobias Gruetzmacher
+# Copyright (C) 2015-2019 Tobias Gruetzmacher
 
 from __future__ import absolute_import, division, print_function
 
 import re
 
 from dosagelib import scraper
+from dosagelib.plugins import old
 
 
 class TestComicNames(object):
@@ -21,3 +22,12 @@ class TestComicNames(object):
             else:
                 comicname = name
             assert re.sub("[^0-9a-zA-Z_]", "", comicname) == comicname
+
+    def test_renamed(self):
+        for scraperobj in scraper.get_scrapers(include_removed=True):
+            if not isinstance(scraperobj, old.Renamed):
+                continue
+            assert len(scraperobj.getDisabledReasons()) > 0
+            # Renamed scraper should only point to an non-disabled scraper
+            newscraper = scraper.find_scrapers(scraperobj.newname)[0]
+            assert len(newscraper.getDisabledReasons()) == 0
