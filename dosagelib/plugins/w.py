2068130ae1bf97242aa72395902dc8a94f648de3
Techwolf <dring@wolfishly.me>
2020-04-16 01:57:12 -0700

    Add WintersLight

--- dosagelib/plugins/w.py
+++ dosagelib/plugins/w.py
@@ -143,6 +143,14 @@ class WildeLife(_ComicControlScraper):
     firstStripUrl = stripUrl % '1'
 
 
+class WintersLight(_ParserScraper):
+    url = 'https://winterslight.anaria.net/'
+    stripUrl = url + 'comic/%s'
+    firstStripUrl = stripUrl % 'winterslight00'
+    imageSearch = '//img[contains(@src, "comic/pages/")]'
+    prevSearch = '//a[contains(text(), "Previous")]'
+
+
 class Wonderella(_BasicScraper):
     url = 'http://nonadventures.com/'
     rurl = escape(url)
