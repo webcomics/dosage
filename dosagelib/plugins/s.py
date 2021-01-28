5339eec876551cf25d4cc297d67792428ac79a8b
Techwolf <dring@wolfishly.me>
2021-01-27 18:05:32 -0800

    Fix Supercell

--- dosagelib/plugins/s.py
+++ dosagelib/plugins/s.py
@@ -672,7 +672,7 @@ class Supercell(_ParserScraper):
     url = 'https://www.supercellcomic.com/'
     stripUrl = url + 'pages/%s.html'
     firstStripUrl = stripUrl % '0001'
-    imageSearch = '//div[@class="comicpage"]//img'
+    imageSearch = '//img[@class="comicStretch"]'
     prevSearch = '//div[@class="comicnav"]/a[./img[contains(@src, "comnav_02")]]'
 
 
