adf1531d4fc95e4452132fe716e03207e33b3eea
Tobias Gruetzmacher <tobias-git@23.gs>
2021-08-31 00:38:58 +0200

    Merge krisstraub comics into a common module

---
+++ dosagelib/plugins/krisstraub.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Tobias Gruetzmacher
+from .common import _WordPressScraper
+
+
+class KrisStraub(_WordPressScraper):
+    prevSearch = '//a[text()="Previous"]'
+    endOfLife = True
+    help = 'Index format: yyyymmdd'
+
+    def __init__(self, name, firstDate):
+        super().__init__(name)
+        self.url = 'https://{}.krisstraub.com/'.format(name.lower())
+        self.stripUrl = self.url + '%s.shtml'
+        self.firstStripUrl = self.stripUrl % firstDate
+
+    @classmethod
+    def getmodules(cls):
+        return (
+            cls('BroodHollow', '20121006'),
+            cls('ChainsawSuit', '20080810'),
+            cls('Starslip', '20050523'),
+        )
