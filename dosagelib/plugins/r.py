18b8a79ab7aa449d5664e586af6994ee4f882bd3
Techwolf <dring@wolfishly.me>
2022-04-06 02:43:47 -0700

    Fix Replay

--- dosagelib/plugins/r.py
+++ dosagelib/plugins/r.py
@@ -116,9 +116,8 @@ class Requiem(_WordPressScraper):
 class Replay(_ParserScraper):
     url = 'http://replaycomic.com/'
     stripUrl = url + 'comic/%s/'
-    url = stripUrl % 'trying-it-out'
     firstStripUrl = stripUrl % 'red-desert'
-    imageSearch = '//div[@id="comic"]//img'
+    imageSearch = '//div[@id="comic"]//img[@alt]'
     prevSearch = '//a[contains(@class, "comic-nav-previous")]'
     nextSearch = '//a[contains(@class, "comic-nav-next")]'
 
