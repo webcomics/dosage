13cb80728b05efd9b5ec199506bc18aeb3be4d59
Techwolf <dring@wolfishly.me>
2022-11-02 18:37:39 -0700

    Add WebToons/CatchMeIfYouCan

--- dosagelib/plugins/webtoons.py
+++ dosagelib/plugins/webtoons.py
@@ -106,6 +106,7 @@ class WebToons(ParserScraper):
             cls('CARL', 'slice-of-life/carl', 1216),
             cls('Caster', 'action/caster', 1461),
             cls('CastleSwimmer', 'fantasy/castle-swimmer', 1499),
+            cls('CatchMeIfYouCan', 'challenge/catch-me-if-you-can-', 434808),
             cls('Catharsis', 'fantasy/catharsis', 396),
             cls('CatLoafAdventures', 'slice-of-life/cat-loaf-adventures', 1381),
             cls('CChansACatgirl', 'challenge/c-chans-a-catgirl', 263430),
