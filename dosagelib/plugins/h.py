392dc094dc25dd35787a512cf623a622f5c7ff20
Tobias Gruetzmacher <tobias-git@23.gs>
2025-05-11 16:50:26 +0200

    Fix Hackles using the Internet Archive

--- dosagelib/plugins/h.py
+++ dosagelib/plugins/h.py
@@ -1,18 +1,19 @@
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2004-2008 Tristan Seligmann and Jonathan Jacobs
-# Copyright (C) 2012-2014 Bastian Kleineidam
-# Copyright (C) 2015-2022 Tobias Gruetzmacher
-# Copyright (C) 2019-2020 Daniel Ring
+# SPDX-FileCopyrightText: © 2004 Tristan Seligmann and Jonathan Jacobs
+# SPDX-FileCopyrightText: © 2012 Bastian Kleineidam
+# SPDX-FileCopyrightText: © 2015 Tobias Gruetzmacher
+# SPDX-FileCopyrightText: © 2019 Daniel Ring
 from re import compile, escape
 
+from ..helpers import bounceStarter, indirectStarter
 from ..scraper import BasicScraper, ParserScraper
 from ..util import tagre
-from ..helpers import bounceStarter, indirectStarter
-from .common import ComicControlScraper, WordPressScraper, WordPressNaviIn
+from .common import ComicControlScraper, WordPressNaviIn, WordPressScraper
 
 
 class Hackles(ParserScraper):
-    url = 'http://hackles.org/'
+    url = ('https://web.archive.org/web/20220128022158/'
+        'http://hackles.org/')
     stripUrl = url + 'cgi-bin/archives.pl?request=%s'
     firstStripUrl = stripUrl % '1'
     imageSearch = '//img[contains(@src, "strips/")]'
