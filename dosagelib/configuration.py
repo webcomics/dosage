074a59fad3bb14b95c759cfae00534ca80f1f877
Tobias Gruetzmacher <tobias-git@23.gs>
2025-05-17 23:56:40 +0200

    Clean up configuration strings

--- dosagelib/configuration.py
+++ dosagelib/configuration.py
@@ -1,26 +1,25 @@
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2012-2014 Bastian Kleineidam
-# Copyright (C) 2015-2020 Tobias Gruetzmacher
-# Copyright (C) 2019-2020 Daniel Ring
+# SPDX-FileCopyrightText: © 2012 Bastian Kleineidam
+# SPDX-FileCopyrightText: © 2015 Tobias Gruetzmacher
+# SPDX-FileCopyrightText: © 2019 Daniel Ring
 """
 Define basic configuration data like version or application name.
 """
 from . import AppName, __version__
 
-App = AppName + u' ' + __version__
+App = f'{AppName} {__version__}'
 
-Maintainer = u'Tobias Gruetzmacher'
-MaintainerEmail = u'tobias-dosage@23.gs'
-Url = u'https://dosage.rocks/'
-SupportUrl = u'https://github.com/webcomics/dosage/issues'
-UserAgent = u"Mozilla/5.0 (compatible; %s/%s; +%s)" % (AppName, __version__,
-                                                       Url)
-Copyright = u"""Copyright (C) 2004-2008 Tristan Seligmann and Jonathan Jacobs
+Maintainer = 'Tobias Gruetzmacher'
+MaintainerEmail = 'tobias-dosage@23.gs'
+Url = 'https://dosage.rocks/'
+SupportUrl = 'https://github.com/webcomics/dosage/issues'
+UserAgent = f"Mozilla/5.0 (compatible; {AppName}/{__version__}; +{Url})"
+Copyright = """Copyright (C) 2004-2008 Tristan Seligmann and Jonathan Jacobs
 Copyright (C) 2012-2014 Bastian Kleineidam
-Copyright (C) 2015-2020 Tobias Gruetzmacher
+Copyright (C) 2015-2025 Tobias Gruetzmacher
 Copyright (C) 2019-2020 Daniel Ring
 """
-Freeware = AppName + u""" comes with ABSOLUTELY NO WARRANTY!
+Freeware = f"""{AppName} comes with ABSOLUTELY NO WARRANTY!
 This is free software, and you are welcome to redistribute it
 under certain conditions. Look at the file `COPYING' within this
 distribution."""
